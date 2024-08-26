c7e91e0facf3a693fdacd4620bac860252ee248f
BlasixMac <blasixcorp@gmail.com>
2024-08-26 21:23:01 +0200

    made the speedometer circle

--- lib/speedometer/speed_screen.dart
+++ lib/speedometer/speed_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
@@ -10,7 +12,7 @@ class LocationServiceWidget extends StatefulWidget {
 
 class _LocationServiceWidgetState extends State<LocationServiceWidget> {
   // bools later for other files
-  bool isKMPH = false;
+  bool isKMPH = true;
   bool isDarkMode = false;
 
   Location location = Location();
@@ -90,21 +92,29 @@ class _LocationServiceWidgetState extends State<LocationServiceWidget> {
                       alignment: Alignment.center,
                       children: [
                         Speedometer(
-                            speed: _locationData!.speed!,
+                            speed: _convertSpeed(_locationData!.speed!) * 1.0,
                             maxSpeed: 240,
                             isKMPH: isKMPH),
                         Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Text(
-                              isKMPH ? "km/h" : "mph",
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              "${_convertSpeed(_locationData!.speed!)}",
-                              style: const TextStyle(
-                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            const SizedBox(),
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  isKMPH ? "km/h" : "mph",
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "${_convertSpeed(_locationData!.speed!)}",
+                                  style: const TextStyle(
+                                      fontSize: 52,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
                             ),
                             const Text("Distance")
                           ],
@@ -154,46 +164,88 @@ class Speedometer extends StatelessWidget {
   final double maxSpeed;
   final bool isKMPH;
 
-  const Speedometer(
-      {super.key,
-      required this.speed,
-      required this.maxSpeed,
-      required this.isKMPH});
+  const Speedometer({
+    super.key,
+    required this.speed,
+    required this.maxSpeed,
+    required this.isKMPH,
+  });
+
   @override
   Widget build(BuildContext context) {
-    double x = 240;
-    double y = 12;
-    double percent;
-    if (speed != 0) {
-      percent = (speed / (maxSpeed + speed)) * x;
-    } else {
-      percent = 0;
-    }
-    return Stack(
-      children: [
-        Container(
-          width: x,
-          height: y,
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.circular(35),
-          ),
-        ),
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 500),
-          width: percent,
-          height: y,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: speed == 0
-                ? BorderRadius.circular(35)
-                : const BorderRadius.only(
-                    topLeft: Radius.circular(35),
-                    bottomLeft: Radius.circular(35),
-                  ),
-          ),
-        ),
-      ],
+    return CustomPaint(
+      size: const Size(200, 200), // Adjust size according to your design
+      painter: SpeedometerPainter(speed: speed, maxSpeed: maxSpeed),
     );
+  }
+}
+
+class SpeedometerPainter extends CustomPainter {
+  final double speed;
+  final double maxSpeed;
+
+  SpeedometerPainter({required this.speed, required this.maxSpeed});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Define the size of the speedometer
+    double radius = min(size.width / 2, size.height / 2);
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double startAngle = 3 * pi / 4;
+    double sweepAngle = 3 * pi / 2;
+    double currentAngle = startAngle + (sweepAngle * (speed / maxSpeed));
+
+    // Draw the background arc
+    Paint arcPaint = Paint()
+      ..color = Colors.grey.shade300
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15;
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      startAngle,
+      sweepAngle,
+      false,
+      arcPaint,
+    );
+
+    // Draw the filled arc (representing speed)
+    Paint progressPaint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15;
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      startAngle,
+      currentAngle - startAngle,
+      false,
+      progressPaint,
+    );
+
+    // // Draw the speed indicator (needle)
+    // Paint needlePaint = Paint()
+    //   ..color = Colors.red
+    //   ..strokeWidth = 2;
+
+    // double needleLength = radius - 15;
+    // Offset needleEnd = Offset(
+    //   center.dx + needleLength * cos(currentAngle),
+    //   center.dy + needleLength * sin(currentAngle),
+    // );
+
+    // canvas.drawLine(center, needleEnd, needlePaint);
+
+    // // Draw the center circle
+    // Paint centerCirclePaint = Paint()
+    //   ..color = Colors.black
+    //   ..style = PaintingStyle.fill;
+
+    // canvas.drawCircle(center, 5, centerCirclePaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
